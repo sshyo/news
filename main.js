ebcb9b2e42032ff4fd7742917e5458174636ad88
Hyojung <hyo8967@naver.com>
2024-02-20 22:04:17 +0900

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY= `39c0d0315a8b48c3842b1798a34a8c6a`;
 let news=[]
 const getLatestNews = async ()=>{
-  const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  const url = new URL (`https://main--capable-pixie-8d3cc6.netlify.app/=${API_KEY}`);
   const response = await fetch(url);
   const data = await response.json()
   news = data.articles;
